d51bf8a0c216b278c14bdf0020f3ac87d22339ba
Sammy Ech-Chaouy <sech-chaouy@aclanyc.com>
2017-05-04 17:15:58 +0000

    Random

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
+    
+    var group = ["Sammy", "Joel", "luis", "julio", "threeshar",];
+    
+    var ScriptEducators = ["Julia", "Alyxe""Aaron", "Justin", "Zach",];
+    
+    $("#studentButton").click(function() {
+       
+        var students = Math.floor(Math.random()*group.length);
+       
+        $("#studentDisplay").append(group[students]);
 
+    });
 
-
+$("#teacherButton").click(function() {
+    
+    var teachers = Math.floor(Math.random()*ScriptEducators.length);
+  
+    $("#teacherDisplay").append(ScriptEducators[teachers]);
+});
+    
 });
